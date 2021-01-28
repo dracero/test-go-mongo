161e83964da1ff90e3d5c2a56a3c2671d383e1eb
dracero <diego.racero@gmail.com>
2021-01-28 19:56:11 -0300

    Update main.go

--- main.go
+++ main.go
@@ -28,3 +28,45 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8000", r))
 
 }
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// we created Book array
+	var books []models.Book
+
+	// bson.M{},  we passed empty filter. So we want to get all data.
+	cur, err := collection.Find(context.TODO(), bson.M{})
+
+	if err != nil {
+		helper.GetError(err, w)
+		return
+	}
+
+	// Close the cursor once finished
+	/*A defer statement defers the execution of a function until the surrounding function returns.
+	simply, run cur.Close() process but after cur.Next() finished.*/
+	defer cur.Close(context.TODO())
+
+	for cur.Next(context.TODO()) {
+
+		// create a value into which the single document can be decoded
+		var book models.Book
+		// & character returns the memory address of the following variable.
+		err := cur.Decode(&book) // decode similar to deserialize process.
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// add item our array
+		books = append(books, book)
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(w).Encode(books) // encode similar to serialize process.
+}
+
+
